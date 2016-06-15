4b6892117e950842da86418539426dddf8beb981
DSadowski3 <dsadowski2001@yahoo.com>
2016-06-14 20:51:12 -0500

    Initial Commit

---
+++ ResumeApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResumeApplication
+//
+//  Created by Dominik Sadowski on 6/14/16.
+//  Copyright © 2016 Dominik Sadowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
